700582d5bb1b30a0ea60da620fb6c5576615bace
Yuta Futamata <uc00z.rayss.gm@gmail.com>
2018-11-11 11:31:10 +0900

    新規開発

---
+++ calculator.py
@@ -0,0 +1,13 @@
+print("2つの数値の計算を行います")
+menu = input("メニュー 1:足し算 2：引き算 3：掛け算 4：割り算 >>")
+
+if menu == 1:
+	print()
+elif menu == 2:
+	print()
+elif menu == 3:
+	print()
+elif menu == 4:
+	print()
+else :
+	print("エラー：入力できる数値は1~4です")
