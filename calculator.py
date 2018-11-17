7a5d59d0da02ee07da93eddaa819f91820117bff
Yuta Futamata <uc00z.rayss.gm@gmail.com>
2018-11-17 18:29:38 +0900

    二乗機能追加

--- calculator.py
+++ calculator.py
@@ -1,7 +1,11 @@
 import math
 
+def square(x):
+	ans = x * x
+	return ans
+
 print("2つの数値の計算を行います")
-menu = input("メニュー 1:足し算 2：引き算 3：掛け算 4：割り算 5：√ >> ")
+menu = input("メニュー 1:足し算 2：引き算 3：掛け算 4：割り算 5：√ 6：二乗 >> ")
 x    = input("数値1(整数) >> ")
 y    = input("数値2(整数) >> ") 
 
@@ -19,6 +23,11 @@ elif menu == '4':
 elif menu == '5':
 	answer_x = math.sqrt(int(x))
 	answer_y = math.sqrt(int(y))
+	print('数値1:',answer_x)
+	print('数値2:',answer_y)
+elif menu == '6':
+	answer_x = square(int(x))
+	answer_y = square(int(y))
 	print('数値1:',answer_x)
 	print('数値2:',answer_y)
 else:
