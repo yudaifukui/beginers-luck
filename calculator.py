688506f7e21dac0e6f86172b33dd12d6c15bf088
Yuta Futamata <uc00z.rayss.gm@gmail.com>
2018-11-17 16:41:35 +0900

    数値入力、足し算機能追加

--- calculator.py
+++ calculator.py
@@ -1,13 +1,16 @@
 print("2つの数値の計算を行います")
 menu = input("メニュー 1:足し算 2：引き算 3：掛け算 4：割り算 >>")
+x    = input("数値1 >> ")
+y    = input("数値2 >> ") 
 
-if menu == 1:
+if menu == '1':
+	sum = int(x) + int(y)
+	print(sum)
+elif menu == '2':
 	print()
-elif menu == 2:
+elif menu == '3':
 	print()
-elif menu == 3:
+elif menu == '4':
 	print()
-elif menu == 4:
-	print()
-else :
+else:
 	print("エラー：入力できる数値は1~4です")
