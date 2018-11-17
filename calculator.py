36ff09e4d419f313587039dddc84fc25553a9dd6
Yuta Futamata <uc00z.rayss.gm@gmail.com>
2018-11-17 18:11:50 +0900

    √機能追加、それに合わせて全体も変更

--- calculator.py
+++ calculator.py
@@ -1,8 +1,11 @@
-print("2つの数値の計算を行います")
-menu = input("メニュー 1:足し算 2：引き算 3：掛け算 4：割り算 >>")
-x    = input("数値1 >> ")
-y    = input("数値2 >> ") 
+import math
 
+print("2つの数値の計算を行います")
+menu = input("メニュー 1:足し算 2：引き算 3：掛け算 4：割り算 5：√ >> ")
+x    = input("数値1(整数) >> ")
+y    = input("数値2(整数) >> ") 
+
+print('\n結果')
 if menu == '1':
 	answer1 = int(x) + int(y)
 	print(answer1)
@@ -13,5 +16,10 @@ elif menu == '3':
 	print(answer3)
 elif menu == '4':
 	print()
+elif menu == '5':
+	answer_x = math.sqrt(int(x))
+	answer_y = math.sqrt(int(y))
+	print('数値1:',answer_x)
+	print('数値2:',answer_y)
 else:
-	print("エラー：入力できる数値は1~4です")
+	print("エラー：入力できる数値は1~5です")
